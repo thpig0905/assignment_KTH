f7cfcde4741eec43942c9b0ee40a3d80b3433397
thpig0905 <kth090567@gmail.com>
2023-12-16 00:06:48 +0900

    2023/12/16 00:00 first release 2023/12/16 00:06 Inpput first release

--- Shop/src/Utils/InnputManger.java
+++ Shop/src/Utils/InnputManger.java
@@ -1,5 +1,28 @@
 package Utils;
 
-public class InnputManger {
+import java.util.Scanner;
 
+public class InnputManger {
+    Scanner sc = new Scanner(System.in);
+
+    public int getNumber(String msg, int min, int max) {
+        while (true) {
+            try {
+                int num = Integer.parseInt(sc.nextLine());
+                if (num < min || num > max){
+                    System.out.println("잘못된 입력입니다.");
+                    continue;
+                }
+                sc.nextLine();
+                return num;
+            } catch (Exception e) {
+                System.out.println("잘못된 입력입니다.");
+                sc.nextLine();
+            }
+        }
+    }
+    public String getString(String msg) {
+        System.out.println(msg);
+        return sc.nextLine();
+    }
 }
