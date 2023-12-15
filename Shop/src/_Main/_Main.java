ae9799cd4b206ddac61120e3a7b054ac6d87c9e9
thpig0905 <kth090567@gmail.com>
2023-12-16 00:21:09 +0900

    2023/12/16 00:00 first release 2023/12/16 00:06 Inpput first release 2023/12/16 00:10 Import Util.InputManger Test.1 2023/12/16 00:20 Main Test.1

--- Shop/src/_Main/_Main.java
+++ Shop/src/_Main/_Main.java
@@ -1,9 +1,11 @@
 package _Main;
 
+import Controller.ShopController;
+
 public class _Main {
 
 	public static void main(String[] args) {
-
+		ShopController shopController = new ShopController();
+		shopController.run();
 	}
-
 }
