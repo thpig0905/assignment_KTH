a056934d8c7217e26219d2291bba3d04bbd99a1f
thpig0905 <kth090567@gmail.com>
2023-12-16 10:17:12 +0900

    2023/12/16 10:16 FileManger Update ver1

--- Shop/src/Utils/FileManager.java
+++ Shop/src/Utils/FileManager.java
@@ -1,7 +1,26 @@
 package Utils;
 
 public class FileManager {
-  // cart.txt
-	// user.txt
-	// item.txt
+    String fileName_cart = "cart.txt";
+    String fileName_user = "user.txt";
+    String fileName_item = "item.txt";
+
+    public void saveCart() {
+
+    }
+    public void loadCart() {
+
+    }
+    public void saveUser() {
+
+    }
+    public void loadUser() {
+
+    }
+    public void saveItem() {
+
+    }
+    public void loadItem() {
+
+    }
 }
